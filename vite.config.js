ee76b96730d2e778b5ba42820e7d86baed010d67
Jagruthi <jagruthireddy321@gmail.com>
2025-06-04 23:40:44 +0530

    changes vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // 👈 must match your GitHub repo name exactly (case-sensitive)
+  base: './',  // Use relative paths so assets load correctly on Vercel
 });
